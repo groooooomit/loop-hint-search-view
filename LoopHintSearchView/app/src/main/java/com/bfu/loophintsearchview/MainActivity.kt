f37dd0fb0d23eaeab30840a2c413eca2251e5a6b
fubo <fubofubo@hotmail.com>
2022-11-07 23:50:07 +0800

    mockData

--- LoopHintSearchView/app/src/main/java/com/bfu/loophintsearchview/MainActivity.kt
+++ LoopHintSearchView/app/src/main/java/com/bfu/loophintsearchview/MainActivity.kt
@@ -2,10 +2,33 @@ package com.bfu.loophintsearchview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.bfu.loophintsearchview.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        mockData()
+    }
+
+    private fun mockData() {
+        lifecycleScope.launch {
+            while (isActive) {
+                delay(2_000)
+                val data1 = listOf(1, 2, 3, 4, 5, 6, 7).map { it.toString() }
+                binding.searchView.updateHint(data1)
+                delay(6_000)
+                val data2 = listOf("A", "B", "C", "D", "E", "F", "G")
+                binding.searchView.updateHint(data2)
+                delay(10_000)
+            }
+        }
     }
 }
