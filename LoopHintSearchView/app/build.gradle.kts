9acbbb8e238b760424e229089bc1504ec87a5fa2
fubo <fubofubo@hotmail.com>
2022-11-30 23:37:52 +0800

    add activity-ktx

--- LoopHintSearchView/app/build.gradle.kts
+++ LoopHintSearchView/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity-ktx:1.6.1")
 
     /* lifecycle */
     val lifecycleVersion = "2.5.1"
