322611559c243f360d09715739075a54656e39ac
fubo <fubofubo@hotmail.com>
2023-04-15 04:25:23 +0800

    add test

--- LoopHintSearchView/app/src/androidTest/java/com/bfu/loophintsearchview/AndroidMainTest.kt
+++ LoopHintSearchView/app/src/androidTest/java/com/bfu/loophintsearchview/AndroidMainTest.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -16,34 +18,62 @@ import kotlin.coroutines.resume
 @RunWith(AndroidJUnit4::class)
 class AndroidMainTest {
 
-    private val flag = MutableLiveData(false)
+    ///////////////////////////////////////////////////////////////////////////
+    // livedata
+    ///////////////////////////////////////////////////////////////////////////
 
-    private suspend fun awaitFlagUntilTrue() = suspendCancellableCoroutine { cont ->
+    private val flagLiveData = MutableLiveData(false)
+
+    private suspend fun awaitFlagUntilTrueByLiveData() = suspendCancellableCoroutine { cont ->
         val observer = object : Observer<Boolean> {
             override fun onChanged(value: Boolean) {
                 if (value) {
-                    flag.removeObserver(this)
+                    flagLiveData.removeObserver(this)
                     // flag now is True
                     cont.resume(Unit)
                 }
             }
         }
         cont.invokeOnCancellation {
-            flag.removeObserver(observer)
+            flagLiveData.removeObserver(observer)
         }
-        flag.observeForever(observer)
+        flagLiveData.observeForever(observer)
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // flow
+    ///////////////////////////////////////////////////////////////////////////
+
+    private val flagFlow = MutableStateFlow(false)
+
+    private suspend fun awaitFlagUntilTrueByFlow() {
+        flagFlow.firstOrNull { it }
     }
 
     @Test
-    fun testAwaitFlag(): Unit = runBlocking(Dispatchers.Main.immediate) {
-        logD { "test begin, flag.value = ${flag.value}" }
+    fun testAwaitFlagUntilTrueByLiveData(): Unit = runBlocking(Dispatchers.Main.immediate) {
+        logD { "testAwaitFlagUntilTrueByLiveData begin, flag.value = ${flagLiveData.value}" }
         val c1 = launch {
             delay(2000)
-            flag.value = true
+            flagLiveData.value = true
         }
         val c2 = launch {
-            awaitFlagUntilTrue()
-            logD { "c2, flag.value = ${flag.value}" }
+            awaitFlagUntilTrueByLiveData()
+            logD { "c2, flag.value = ${flagLiveData.value}" }
+        }
+        joinAll(c1, c2)
+    }
+
+    @Test
+    fun testAwaitFlagUntilTrueByFlow(): Unit = runBlocking(Dispatchers.Main.immediate) {
+        logD { "testAwaitFlagUntilTrueByFlow begin, flag.value = ${flagFlow.value}" }
+        val c1 = launch {
+            delay(2000)
+            flagFlow.value = true
+        }
+        val c2 = launch {
+            awaitFlagUntilTrueByFlow()
+            logD { "c2, flag.value = ${flagFlow.value}" }
         }
         joinAll(c1, c2)
     }
